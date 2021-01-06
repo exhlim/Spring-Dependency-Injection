ea0ae54caab8e68f219fc109b93f765c6fe5dcae
exhlim <exhlimm@gmail.com>
2021-01-06 13:24:15 +0800

    Testing token

--- src/main/java/com/exhlim/springdependencyinjection/services/I18nSpanishGreetingService.java
+++ src/main/java/com/exhlim/springdependencyinjection/services/I18nSpanishGreetingService.java
@@ -3,8 +3,10 @@ package com.exhlim.springdependencyinjection.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-/** Here service takes in a qualifier **/
-@Profile("ES")
+/** Here service takes in a qualifier
+ * profile can take more than 1 active profile
+ * **/
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
 	@Override
